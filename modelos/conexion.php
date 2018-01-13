355dfd94ac5a291c6e422ed8f2f7580bb9ae6074
isaacsibaja <isibajar91@hotmail.com>
2018-01-13 13:54:41 -0600

    POS
    Sistema Punto de Ventas v1

---
+++ modelos/conexion.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*=============================================
+=            CLASE CONEXION A LA BD           =
+=============================================*/
+
+
+class Conexion {
+	
+	public function conectar () {
+
+		$link = new PDO("mysql:host=localhost;dbname=pos","root","");
+
+		$link->exec("set names utf8");
+
+		return $link;
+
+	}
+}
+
+
+ ?>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+
+	<strong>Copyright &copy; 2018 <a href="#" target="_blank">Isaac Sibaja</a></strong>
+	Todos los derechos reservados.
+
+	
+</footer>
